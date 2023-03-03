be681400505a1bb08c69ea0a8ec14eb7b0b414b8
Federico <as>
2023-03-03 15:57:45 +0000

    fixed navbar menu bugs

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -5,85 +5,86 @@ class NavBar extends HTMLElement {
     }
 
     connectedCallback() {
+        // get elements
+        this.menuButton = this.querySelector('.menu-button')
+        this.linksContainer = this.querySelector('.links-container')
+        this.mainSection = document.querySelector('main')
+        this.logo = this.querySelector('svg')
+
+        // add event listeners
+        this.logo.addEventListener('click', () => { this.loadPage('home') })
+        this.menuButton.addEventListener('click', () => { this.toggleMenu() })
+        this.mainSection.addEventListener('click', () => { this.closeMenu() })
+        window.addEventListener('resize', () => { if (window.innerWidth > 768) { this.closeMenu() } })
+
         // create pages
-        const pages = ['home', 'baking', 'contact'];
+        const pages = ['home', 'baking', 'contact']
         const linksContainer = this.querySelector('.links-container')
         pages.forEach(page => {
-            const link = document.createElement('a');
-            link.innerHTML = page;
+            const link = document.createElement('a')
+            link.innerHTML = page
             link.addEventListener('click', () => {
-                this.loadPage(page);
-            });
-            linksContainer.appendChild(link);
-        });
+                this.loadPage(page)
+            })
+            linksContainer.appendChild(link)
+        })
 
         // set first page
-        const params = new URLSearchParams(window.location.search);
-        const target = params.get('page');
-        this.loadPage(target);
-
-        // add event listener for logo
-        const logo = this.querySelector('svg')
-        logo.addEventListener('click', () => {
-            this.loadPage('home');
-        });
-
-        // add event listener for menu button
-        const menu = this.querySelector('.menu-button');
-        menu.addEventListener('click', () => {
-            this.openCloseMenu();
-        });
-    }
-
-    openCloseMenu() {
-        const menu = this.querySelector('.menu-button');
-        const linksContainer = this.querySelector('.links-container');
-        menu.classList.toggle('menu-button-open');
-        linksContainer.classList.toggle('links-container-open');
+        const params = new URLSearchParams(window.location.search)
+        const target = params.get('page')
+        this.loadPage(target)
     }
 
     loadPage(page) {
         // reformat page name
-        var target = page || 'home'; // === TO CHANGE IN PRODUCTION ===
-        target = target.charAt(0) === '/' ? target.substring(1) : target;
+        var target = page || 'home' // === TO CHANGE IN PRODUCTION ===
+        target = target.charAt(0) === '/' ? target.substring(1) : target
 
-        // get content and update url         
-        this.main = document.querySelector('main');
-        window.history.pushState({}, '', target); // === TO CHANGE IN PRODUCTION ===
+        // update url         
+        // window.history.pushState({}, '', target) // === TO CHANGE IN PRODUCTION ===
 
         // load new page with fade effect
-        this.main.style.opacity = 0;
+        this.mainSection.style.opacity = 0
         setTimeout(() => {
-            this.loadHtml(`/html/${target}.html`);
-            // fade in
-            this.main.style.opacity = 1
-        }, 400);
+            this.loadHtml(`/html/${target}.html`)
+            this.mainSection.style.opacity = 1
+        }, 400)
 
         // set active link
-        const links = this.querySelectorAll('a');
+        const links = this.querySelectorAll('a')
         links.forEach(link => {
             if (link.innerHTML === target) {
-                link.classList.add('selected');
+                link.classList.add('selected')
             } else {
-                link.classList.remove('selected');
+                link.classList.remove('selected')
             }
-        });
+        })
 
         // close menu
-        this.openCloseMenu();
+        this.closeMenu()
     }
 
     loadHtml(path) {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', path, true);
+        const xhr = new XMLHttpRequest()
+        xhr.open('GET', path, true)
         xhr.onload = () => {
             if (xhr.status === 200) {
-                this.main.innerHTML = xhr.responseText;
+                this.mainSection.innerHTML = xhr.responseText
             } else if (xhr.status === 404) {
-                this.main.innerHTML = 'Page not found';
+                this.mainSection.innerHTML = 'Page not found'
             }
         }
-        xhr.send();
+        xhr.send()
+    }
+
+    toggleMenu() {
+        this.menuButton.classList.toggle('menu-button-open')
+        this.linksContainer.classList.toggle('links-container-open')
+    }
+
+    closeMenu() {
+        this.menuButton.classList.remove('menu-button-open')
+        this.linksContainer.classList.remove('links-container-open')
     }
 }
-window.customElements.define('nav-bar', NavBar);
+window.customElements.define('nav-bar', NavBar)
