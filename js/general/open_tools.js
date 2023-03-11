edc7464ab9224b30c97910cf0e4f25d8219fc9a5
Federico <as>
2023-03-11 15:16:37 +0000

    optimized open tools

--- js/general/open_tools.js
+++ js/general/open_tools.js
@@ -1,27 +1,13 @@
-// open pomodoro timer
-export function openPomodoro() {
-    fetch(`/html/pomodoro.html`)
+// open tools
+export function openTools(name, width, height) {
+    fetch(`/html/${name}.html`)
         .then(response => {
             return response.text()
         })
         .then(html => {
-            const pomodoroWindow = window.open("", "", "width=300,height=400,top=100,left=100,menubar=no,toolbar=no,location=no,resizable=yes,scrollbars=yes")
-            pomodoroWindow.document.write(html)
-            pomodoroWindow.history.pushState({}, '', 'pomodoro')
-            pomodoroWindow.document.close()
-        })
-}
-
-// open baking tools
-export function openBaking() {
-    fetch(`/html/baking.html`)
-        .then(response => {
-            return response.text()
-        })
-        .then(html => {
-            const bakingWindow = window.open("", "", "width=500,height=600,top=100,left=100,menubar=no,toolbar=no,location=no,resizable=yes,scrollbars=yes")
+            const bakingWindow = window.open("", "", `width=${width},height=${height},top=50,left=100,menubar=no,toolbar=no,location=no,resizable=yes,scrollbars=yes`)
             bakingWindow.document.write(html)
-            bakingWindow.history.pushState({}, '', 'baking')
+            bakingWindow.history.pushState({}, '', name)
             bakingWindow.document.close()
         })
 }
