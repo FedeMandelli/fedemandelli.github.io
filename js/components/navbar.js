a443a0c84ca373f55a4f7c94e98eb60e7b10e4e3
Federico <as>
2024-09-12 14:47:44 +0100

    moved tools to own page and added state observer for history navigation

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -1,98 +1,113 @@
 // === Navigation Bar ===
 class NavBar extends HTMLElement {
-    constructor() {
-        super()
-    }
+  constructor() {
+    super();
+  }
 
-    connectedCallback() {
-        // get elements
-        this.menuButton = this.querySelector('.menu-button')
-        this.linksContainer = this.querySelector('.links-container')
-        this.mainSection = document.querySelector('main')
-        this.logo = this.querySelector('svg')
+  connectedCallback() {
+    // get elements
+    this.menuButton = this.querySelector(".menu-button");
+    this.linksContainer = this.querySelector(".links-container");
+    this.mainSection = document.querySelector("main");
+    this.logo = this.querySelector("svg");
 
-        // add event listeners
-        this.logo.addEventListener('click', () => { this.loadPage('home') })
-        this.menuButton.addEventListener('click', () => { this.toggleMenu() })
-        this.mainSection.addEventListener('click', () => { this.closeMenu() })
-        window.addEventListener('resize', () => { if (window.innerWidth > 768) { this.closeMenu() } })
+    // add event listeners
+    this.logo.addEventListener("click", () => {
+      this.loadPage("home");
+    });
+    this.menuButton.addEventListener("click", () => {
+      this.toggleMenu();
+    });
+    this.mainSection.addEventListener("click", () => {
+      this.closeMenu();
+    });
+    window.addEventListener("resize", () => {
+      if (window.innerWidth > 768) {
+        this.closeMenu();
+      }
+    });
+    window.addEventListener("popstate", () => {
+      this.loadPage();
+    });
 
-        // create pages
-        const pages = ['home', 'contact']
-        const linksContainer = this.querySelector('.links-container')
-        pages.forEach(page => {
-            const link = document.createElement('a')
-            link.innerHTML = page
-            link.addEventListener('click', () => {
-                this.loadPage(page)
-            })
-            linksContainer.appendChild(link)
+    // create pages
+    const pages = ["home", "contact", "tools"];
+    const linksContainer = this.querySelector(".links-container");
+    pages.forEach((page) => {
+      const link = document.createElement("a");
+      link.innerHTML = page;
+      link.addEventListener("click", () => {
+        this.loadPage(page);
+      });
+      linksContainer.appendChild(link);
+    });
+
+    // set first page
+    const params = new URLSearchParams(window.location.search);
+    const target = params.get("page");
+    this.loadPage(target);
+  }
+
+  loadPage(page) {
+    // reformat page name
+    var target = page || "home"; // === SET TO HOME IN PRODUCTION ===
+    target = target.charAt(0) === "/" ? target.substring(1) : target;
+
+    // fade out current page
+    this.mainSection.style.opacity = 0;
+
+    // fetch html
+    setTimeout(() => {
+      fetch(`/html/${target}.html`)
+        .then((response) => {
+          // page found
+          if (response.status === 200) {
+            return response.text();
+          }
+
+          // page not found
+          else if (response.status === 404) {
+            return "Page not found";
+          }
         })
+        .then((html) => {
+          // set page content
+          this.mainSection.innerHTML = "";
+          this.mainSection.appendChild(
+            document.createRange().createContextualFragment(html)
+          );
 
-        // set first page
-        const params = new URLSearchParams(window.location.search)
-        const target = params.get('page')
-        this.loadPage(target)
-    }
-
-    loadPage(page) {
-        // reformat page name
-        var target = page || 'home' // === SET TO HOME IN PRODUCTION ===
-        target = target.charAt(0) === '/' ? target.substring(1) : target
-
-        // fade out current page
-        this.mainSection.style.opacity = 0
-
-        // fetch html
-        setTimeout(() => {
-            fetch(`/html/${target}.html`)
-                .then(response => {
-                    // page found
-                    if (response.status === 200) {
-                        return response.text()
-                    }
-
-                    // page not found
-                    else if (response.status === 404) {
-                        return 'Page not found'
-                    }
-                })
-                .then(html => {
-                    // set page content
-                    this.mainSection.innerHTML = ''
-                    this.mainSection.appendChild(document.createRange().createContextualFragment(html))
-
-                    // update url         
-                    window.history.pushState({}, '', target) // === TO CHANGE IN PRODUCTION ===
-                })
-                .then(() => {
-                    // fade in new page
-                    this.mainSection.style.opacity = 1
-                })
-        }, 400);
-
-        // set active link
-        const links = this.querySelectorAll('a')
-        links.forEach(link => {
-            if (link.innerHTML === target) {
-                link.classList.add('selected')
-            } else {
-                link.classList.remove('selected')
-            }
+          // update url
+          window.history.pushState({}, "", target); // === TO CHANGE IN PRODUCTION ===
         })
+        .then(() => {
+          // fade in new page
+          this.mainSection.style.opacity = 1;
+        });
+    }, 400);
 
-        // close menu
-        this.closeMenu()
-    }
+    // set active link
+    const links = this.querySelectorAll("a");
+    links.forEach((link) => {
+      if (link.innerHTML === target) {
+        link.classList.add("selected");
+      } else {
+        link.classList.remove("selected");
+      }
+    });
 
-    toggleMenu() {
-        this.menuButton.classList.toggle('menu-button-open')
-        this.linksContainer.classList.toggle('links-container-open')
-    }
+    // close menu
+    this.closeMenu();
+  }
 
-    closeMenu() {
-        this.menuButton.classList.remove('menu-button-open')
-        this.linksContainer.classList.remove('links-container-open')
-    }
+  toggleMenu() {
+    this.menuButton.classList.toggle("menu-button-open");
+    this.linksContainer.classList.toggle("links-container-open");
+  }
+
+  closeMenu() {
+    this.menuButton.classList.remove("menu-button-open");
+    this.linksContainer.classList.remove("links-container-open");
+  }
 }
-window.customElements.define('nav-bar', NavBar)
+window.customElements.define("nav-bar", NavBar);
